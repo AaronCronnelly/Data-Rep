8c9cb765b6c525d5fcc46e3721989a07d68f578a
AaronCronnelly <G00403528@gmit.ie>
2023-10-06 15:23:43 +0100

    added the navbar, browerRouter, routes and route

--- Lab2/my-app/src/App.js
+++ Lab2/my-app/src/App.js
@@ -3,28 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import Header from './components/Header';
-// import Content from './components/Content';
-// import Footer from './components/Footer';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
          <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/create">create</Nav.Link>
+            <Nav.Link href="/Content">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      
+
+      <Routes>
+        <Route path="/" element={<Content></Content>}></Route>
+      </Routes>
+
         {/* <Header></Header>
         <Content></Content>
-        <Footer></Footer> */}
+      <Footer></Footer> */}
     </div>
+    </BrowserRouter>
   );
 }
 

acf72c7b912fa27dc9c9693424f3fd7ec49ddea8
AaronCronnelly <G00403528@gmit.ie>
2023-10-06 15:25:08 +0100

    changes

--- Lab2/my-app/src/App.js
+++ Lab2/my-app/src/App.js
@@ -18,8 +18,8 @@ function App() {
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/create">create</Nav.Link>
-            <Nav.Link href="/Content">Pricing</Nav.Link>
+            <Nav.Link href="/Content">create</Nav.Link>
+            <Nav.Link href="/Footer">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>

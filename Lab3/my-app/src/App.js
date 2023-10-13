26a4e524d20082c4aa291917ce08ee5ffa4d1e76
AaronCronnelly <G00403528@gmit.ie>
2023-10-13 11:02:48 +0100

    working on the lab3 react, need to add comments, and finishing adding the reactbootstrap card

--- Lab3/my-app/src/App.js
+++ Lab3/my-app/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Create from './components/Create';
+import Read from './components/Read';
+import Books from './components/books';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/book">Home</Nav.Link>
+              <Nav.Link href="/Create">Create</Nav.Link>
+              <Nav.Link href="/Read">Read</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+        
+          <Route path='/Create' element={<Create></Create>}></Route>
+          <Route path='/Read' element={<Read></Read>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

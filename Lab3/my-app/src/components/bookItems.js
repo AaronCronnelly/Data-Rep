4d0f9e5821b48aea08874ea12568bb8eba8528b7
AaronCronnelly <G00403528@gmit.ie>
2023-10-15 19:37:11 +0100

    added the cards, but they are not aligned

--- Lab3/my-app/src/components/bookItems.js
+++ Lab3/my-app/src/components/bookItems.js
@@ -1,10 +1,20 @@
-function BookItems(props){
-    return(
+import Card from 'react-bootstrap/Card';
+function BookItems(props) {
+    return (
         // console.log("Bookitems test")
         <div>
-            <h1>{props.myBook.title}</h1>
+            {/* <h1>{props.myBook.title}</h1>
             <img src={props.myBook.thumbnailUrl}></img>
-            <p>{props.myBook.authors}</p>
+            <p>{props.myBook.authors}</p> */}
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={props.myBook.thumbnailUrl} />
+                <Card.Body>
+                    <Card.Title>{props.myBook.title}</Card.Title>
+                    <Card.Text>
+                        {props.myBook.authors}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 

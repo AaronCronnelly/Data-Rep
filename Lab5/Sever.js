d17d74fa046da6f299b3afa7e44855d2db73602b
AaronCronnelly <G00403528@gmit.ie>
2023-10-27 09:35:56 +0100

    L5: got welcome to datarep working

--- Lab5/Sever.js
+++ Lab5/Sever.js
@@ -3,9 +3,17 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/whatever',(req, res)=> { //('/whatever',(req, res) what ever is in the '' is being used for following behind the localhost/
+    res.send('GoodBye')
+})
+
+app.get('/DataRep', (req, res)=>{
+    res.send("Welcome to Data-Rep")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
